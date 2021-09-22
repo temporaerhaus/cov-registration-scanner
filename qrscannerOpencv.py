701406b1f9310a0c6f7b295a0bf51f2240eef566
stk <stefan@shutterworks.org>
2021-09-22 18:37:51 +0200

    switched to pyzbar as qr code decoder

--- qrscannerOpencv.py
+++ qrscannerOpencv.py
@@ -1,15 +1,13 @@
 import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
 import print2
 import time
 
 webcam = cv2.VideoCapture(0)
-qrDecoder = cv2.QRCodeDetector()
 
-while(true):
+while(True):
     (_, im) = webcam.read()
-    (data, _, _) = qrDecoder.detectAndDecode(im)
-    if(data!=''):
-        print2.printR(data)
-        time.sleep(5)
-    
-    
+    decodedObjects = pyzbar.decode(im)
+    for obj in decodedObjects:
+      print(obj.data, "\n")
