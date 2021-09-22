36474c4a97d71f6779989930c17420d993a35d59
stk <stefan@shutterworks.org>
2021-09-22 19:55:12 +0200

    added vcard parser

--- qrscannerOpencv.py
+++ qrscannerOpencv.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
-import print2
+#import print2
 import time
+import vobject
 
 webcam = cv2.VideoCapture(0)
 
@@ -10,4 +11,10 @@ while(True):
     (_, im) = webcam.read()
     decodedObjects = pyzbar.decode(im)
     for obj in decodedObjects:
-      print(obj.data, "\n")
+      #print(obj.data.decode('utf-8'), "\n")
+      v = vobject.readOne  ( obj.data.decode('utf-8') )
+      print(v.n.value)
+      print(v.email.value)
+      print(v.tel.value)
+      for adr in v.contents['adr']:
+         print(adr.value)
